8b87ee0b5034d1ef90160dec94ca26c3a2fc5660
Mitchell Blom <mitchellblom@gmail.com>
2017-06-11 17:41:06 -0500

    auth control and factory

--- javascripts/AppConfig.js
+++ javascripts/AppConfig.js
@@ -1,8 +1,36 @@
+let isAuth = (AuthFactory) => new Promise((resolve, reject) => {
+  if (AuthFactory.isAuthenticated()) {
+    resolve();
+  } else {
+    reject();
+  }
+});
+
+app.run(function($location, $rootScope, FIREBASE_CONFIG, AuthFactory) {
+  firebase.initializeApp(FIREBASE_CONFIG);
+  $rootScope.$on('$routeChangeStart', function(event, currRoute, prevRoute) {
+    var logged = AuthFactory.isAuthenticated();
+    var appTo;
+    if (currRoute.originalPath) {
+      appTo = currRoute.originalPath.indexOf('/landing') !== -1;
+    }
+    if (!appTo && !logged) {
+      event.preventDefault();
+      $location.path('/landing');
+    }
+  });
+});
+
 app.config(function($routeProvider) {
   $routeProvider
     .when('/landing', {
       templateUrl: 'partials/landing.html',
       controller: 'AuthCtrl'
     })
+    .when("/boards/:uid", {
+        templateUrl: "partials/trip-view.html",
+        controller: "TripViewCtrl",
+        resolve: {isAuth}
+      })
     .otherwise('/landing');
 });
--- javascripts/controllers/AuthCtrl.js
+++ javascripts/controllers/AuthCtrl.js
@@ -1,3 +1,68 @@
 app.controller("AuthCtrl", function($location, $rootScope, $scope, AuthFactory, UserFactory) {
 
+	$scope.alerts = [];
+
+	$scope.auth = {
+		templateUrl: 'auth.html',
+		email: "a@a.com",
+		password: "123456"
+	};
+
+	if ($location.path() === '/logout') {
+		AuthFactory.logout();
+		$rootScope.user = {};
+		$location.url('/landing');
+	}
+
+	let logMeIn = () => {
+		AuthFactory.authenticate($scope.auth).then((userCreds) => {
+			return UserFactory.getUser(userCreds.uid);
+		}, (error) => {
+			$scope.alerts[0] = {msg: "Oops! Looks like that username or password isn't recognized."};
+		}).then((user) => {
+			$rootScope.user = user;
+			$location.url(`/trips/${$rootScope.user.uid}`);
+		}).catch((error) => {
+			console.log(error);
+		});
+	};
+
+	let logMeInGoogle = () => {
+		AuthFactory.authenticateGoogle($scope.auth)
+			.then((user) => {
+				$rootScope.user = user;
+				$rootScope.user.username = user.email;
+				$location.url(`/trips/${$rootScope.user.uid}`);
+			}).catch((error) => {
+				console.log(error);
+			});
+	};
+
+	$scope.registerUser = () => {
+		AuthFactory.registerWithEmail($scope.auth).then((didRegister) => {
+			$scope.auth.uid = didRegister.uid;
+			return UserFactory.addUser($scope.auth);
+		}, (error) => {
+			console.log(error);
+		}).then((registerComplete) => {
+			logMeIn();
+		}).catch((error) => {
+			console.log(error);
+		});
+	};
+
+	$scope.loginUser = () => {
+		console.log("loginUser executed");
+		logMeIn();
+	};
+
+	$scope.loginUserGoogle = () => {
+		logMeInGoogle();
+	}
+
+	// $scope.loginPopover = {
+ //    templateUrl: 'loginPopover.html',
+ //    userEmail: ''
+ //  };
+
 });
