9e8790f7d5e74e8388dad7d8a1ca5ae429615a6b
Mitchell Blom <mitchellblom@gmail.com>
2017-06-12 12:14:19 -0500

    toggles, headings, and icons

--- javascripts/controllers/NavBarCtrl.js
+++ javascripts/controllers/NavBarCtrl.js
@@ -1,12 +1,10 @@
 app.controller("NavBarCtrl", function($rootScope, $scope) {
 
 	$scope.openNav = () => {
-		console.log("open nav clicked");
 	  document.getElementById("sideNav").style.width = "250px";
 	};
 
 	$scope.closeNav = () => {
-		console.log("close nav clicked");
 	  document.getElementById("sideNav").style.width = "100px";
 	};
 
