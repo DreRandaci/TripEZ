b0a82083d94f5843fbf62ab0fc673f3a2e2bcd64
Mitchell Blom <mitchellblom@gmail.com>
2017-06-12 10:46:00 -0500

    expandable navbar jq solution

--- javascripts/controllers/NavBarCtrl.js
+++ javascripts/controllers/NavBarCtrl.js
@@ -1,4 +1,13 @@
 app.controller("NavBarCtrl", function($rootScope, $scope) {
 
+	$scope.openNav = () => {
+		console.log("open nav clicked");
+	  document.getElementById("sideNav").style.width = "250px";
+	};
+
+	$scope.closeNav = () => {
+		console.log("close nav clicked");
+	  document.getElementById("sideNav").style.width = "100px";
+	};
 
 });
