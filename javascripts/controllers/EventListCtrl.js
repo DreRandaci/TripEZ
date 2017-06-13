099e15a5f1c6e15b3952490b8e26c6869b5c4cc0
Mitchell Blom <mitchellblom@gmail.com>
2017-06-13 09:00:42 -0500

    events returning and printing

--- javascripts/controllers/EventListCtrl.js
+++ javascripts/controllers/EventListCtrl.js
@@ -1,4 +1,25 @@
-app.controller("EventListCtrl", function($location, $rootScope, $routeParams, $scope, UserFactory) {
+app.controller("EventListCtrl", function($location, $rootScope, $routeParams, $scope, EventFactory, TripFactory) {
 
+	let getSingleTripName = () => {
+    TripFactory.getSingleTripNameFromFB($routeParams.tripId)
+    	.then((tripReturned) => {
+      $scope.trip = tripReturned;
+    }).catch((error) => {
+      console.log("getSingleTripName error", error);
+    });
+  };
+
+  getSingleTripName();
+
+	let getEvents = () => {
+    EventFactory.getEventsFromFB($routeParams.tripId)
+    	.then((events) => {
+      $scope.events = events;
+    }).catch((error) => {
+      console.log("getEvents error", error);
+    });
+  };
+
+  getEvents();
 
 });
