afcbfe03bbc25c9212cb880b7134e9a11465e643
Mitchell Blom <mitchellblom@gmail.com>
2017-06-19 12:02:30 -0500

    event end time edit

--- javascripts/controllers/EventViewCtrl.js
+++ javascripts/controllers/EventViewCtrl.js
@@ -1,35 +1,35 @@
 app.controller("EventViewCtrl", function($location, $routeParams, $scope, BaseFactory, EventFactory, TripFactory) {
 
-	let getSingleEvent = () => {
+  let getSingleEvent = () => {
     EventFactory.getSingleEventFromFB($routeParams.eventId)
-  	.then((event) => {
+    .then((event) => {
       $scope.event = event;
       getTripFromTripId(event.trip);
       getBaseFromBaseId(event.base);
       getBases(event.trip);
-  		pinSingleEvent();
-  	})
-  	.catch((error) => {
+      pinSingleEvent();
+    })
+    .catch((error) => {
       console.log("getSingleEvent error", error);
- 	 	});
+    });
   };
 
   getSingleEvent();
 
-	let getTripFromTripId = (tripId) => {
+  let getTripFromTripId = (tripId) => {
     TripFactory.getSingleTripNameFromFB(tripId)
-  	.then((tripReturned) => {
-    	$scope.trip = tripReturned;
-  	})
-  	.catch((error) => {
-    	console.log("getTripFromTripId error", error);
-  	});
+    .then((tripReturned) => {
+      $scope.trip = tripReturned;
+    })
+    .catch((error) => {
+      console.log("getTripFromTripId error", error);
+    });
   };
 
   let getBaseFromBaseId = (baseId) => {
     BaseFactory.getBaseWithBaseIdFromFB(baseId)
     .then((baseReturned) => {
-    	$scope.currentBase = baseReturned;
+      $scope.currentBase = baseReturned;
     })
     .catch ((error) => {
       console.log("error in getBaseFromBaseId", error);
@@ -47,21 +47,37 @@ app.controller("EventViewCtrl", function($location, $routeParams, $scope, BaseFa
   };
 
   $scope.editEvent = () => {
-		EventFactory.editEventInFB($scope.event)
+    EventFactory.editEventInFB($scope.event)
     .then(() => {
       getSingleEvent();
-	  })
+    })
     .catch((error) => {
       console.log("editEvent error", error);
-	  });
-	};
+    });
+  };
 
-	let map;
+  $scope.mytime = new Date();
+  $scope.hstep = 1;
+  $scope.mstep = 1;
+  $scope.ismeridian = true;
+  
+  $scope.toggleMode = function() {
+    $scope.ismeridian = ! $scope.ismeridian;
+  };
 
-	let pinSingleEvent = () => {
-	  let singleEventToPin = {
-	  	lat: Number($scope.event.latitude), 
-	  	lng: Number($scope.event.longitude)
+  $scope.update = function() {
+    var d = new Date();
+    d.setHours( 14 );
+    d.setMinutes( 0 );
+    $scope.mytime = d;
+  };
+
+  let map;
+
+  let pinSingleEvent = () => {
+    let singleEventToPin = {
+      lat: Number($scope.event.latitude), 
+      lng: Number($scope.event.longitude)
 	  };
 	  let map = new google.maps.Map(document.getElementById('map'), {
 	    zoom: 12,
